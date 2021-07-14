3198267e487f91a3fba90e4fed0f39a65712afa5
Sasha Zaitsau <46328412+SashaZaicev@users.noreply.github.com>
2021-07-14 18:10:52 +0300

    Updates

---
+++ precache-manifest.deeccd3827f9a0f5fd8038ae0aeab483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde39fef3b6c5d7dab3d7108d3049e65",
+    "url": "/to-do-list-ts/index.html"
+  },
+  {
+    "revision": "8fb780aa8f2b9ea28379",
+    "url": "/to-do-list-ts/static/css/main.1f1f0cfe.chunk.css"
+  },
+  {
+    "revision": "0199ee781d7e76465d7e",
+    "url": "/to-do-list-ts/static/js/2.373a2356.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/to-do-list-ts/static/js/2.373a2356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb780aa8f2b9ea28379",
+    "url": "/to-do-list-ts/static/js/main.314d6673.chunk.js"
+  },
+  {
+    "revision": "341c580fc848b639c9e8",
+    "url": "/to-do-list-ts/static/js/runtime-main.024bf5b2.js"
+  }
+]);
